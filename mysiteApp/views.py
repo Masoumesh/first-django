15dc885a7e9385c59aa7352f7aedbace955bbd09
Masoumesh <masoumeshahbazzi@gmail.com>
2025-01-06 16:32:48 +0330

    added general templates  folder
    moved website templates to general folder of templates

--- mysiteApp/views.py
+++ mysiteApp/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return render(request, 'index.html')
+    return render(request, 'templates/index.html')
 
 def about_view(request):
-    return render(request, 'about.html')
+    return render(request, 'templates/about.html')
 
 def contact_view(request):
-    return render(request, 'contact.html')
+    return render(request, 'templates/contact.html')
 # Create your views here.
