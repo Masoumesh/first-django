8de329702af3da904477267cab8384bf44cb1ffc
Masoumesh <masoumeshahbazzi@gmail.com>
2025-01-20 00:12:26 +0330

    database and admin

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 # Create your views here.
 
 
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts= Post.objects.filter(status=1)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request, pid):
+    posts= Post.objects.filter(status=1)
+    post = get_object_or_404(posts, pk=pid)  # This will raise 404 if post doesn't exist or is not published.
+    context = {'post': post}
+    return render(request, 'blog/blog-single.html', context)
 
+def test(request, pid):
+    # posts= Post.objects.get(id=pid)
+    post = get_object_or_404(Post, pk=pid)  # This will raise 404 if post doesn't exist or is not published.
+    context = {'post': post}
+    return render(request, 'test.html', context)
--- mysiteApp/models.py
+++ mysiteApp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Contact(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
@@ -8,3 +10,9 @@ class Contact(models.Model):
     message = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+    
+    
+    class Meta:
+        ordering = ('created_date',)
+    def __str__(self):
+        return self.name 
