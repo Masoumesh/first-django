089c6312b259110608c168a1c33bc2f9d4c6e7ff
Masoumesh <masoumeshahbazzi@gmail.com>
2025-01-28 13:50:05 +0330

    concatenate

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     title = models.CharField(max_length= 255)
     content = models.TextField()
     # tag
-    Category = models.ManyToManyField(Category)
+    category = models.ManyToManyField(Category)
     counted_views = models.IntegerField(default= 0)
     status = models.BooleanField(default= False)
     published_date = models.DateTimeField(null=True) 
@@ -26,6 +26,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def snippets(self):
+        return self.content[:100] +'...'
 
 
 
